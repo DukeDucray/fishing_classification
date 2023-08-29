680511d0f27e346f83b5a5c5790f25fd025c39f0
Philippe Giblot Ducray <pgducray@gmail.com>
2023-08-29 18:43:05 +0400

    added direnv, setup_API.py, Dockerfile working

---
+++ setup_API.py
@@ -0,0 +1,20 @@
+from setuptools import find_packages
+from setuptools import setup
+
+with open("requirements_image.txt") as f:
+    content = f.readlines()
+requirements = [x.strip() for x in content if "git+" not in x]
+
+setup(name='fishing_classification',
+      version="0.0.1",
+      description="First Try",
+      license="MIT",
+      author="Team Fishing",
+      author_email="gs.leungshing@gmail.com",
+      #url="https://github.com/lewagon/taxi-fare",
+      install_requires=requirements,
+      packages=find_packages(),
+      test_suite="tests",
+      # include_package_data: to install data from MANIFEST.in
+      include_package_data=True,
+      zip_safe=False)
