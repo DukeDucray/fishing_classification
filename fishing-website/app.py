8099e715df10e8777450706e8925d135bc6b8be9
Grace Leung Shing <gs.leungshing@gmail.com>
2023-08-29 08:54:09 +0400

    Minor changes

--- fishing-website/app.py
+++ fishing-website/app.py
@@ -1,44 +1,53 @@
 import streamlit as st
-from PIL import Image
 import pandas as pd
+import datetime
+import requests
 
+from PIL import Image
+
+# Title
 st.title('Illegal Fishing')
 
+# Map
 st.markdown('''
 Below is a map of sample fishing events around the world 🗺''')
-
 image = Image.open('../data/output.png')
-
 st.image(image, caption='Fishing events around the World')
 
-st.file_uploader("Please upload the csv of the boat your would like to track")
-
+# Information below map
 st.markdown('''
-Our goal is to predict based on the boat's trajectory events went it was fishing or not as well as predict the fishing gear used by the boat''')
+Our goal is to predict based on the boat's trajectory events went it was fishing
+or not as well as predict the fishing gear used by the vessel''')
+
+# Upload csv information
+st.set_option('deprecation.showfileUploaderEncoding', False)
+uploaded_file = st.file_uploader("Choose a CSV file", type="csv")
+if uploaded_file is not None:
+    data = pd.read_csv(uploaded_file)
+    st.write(data)
+
+# Select specific location
+date_time_position = st.date_input("Set date", value=datetime.datetime(2019,7,6))
 boat_longitude = st.number_input('Current Boat\'s Longitude', value=-73.950655, step=1e-7, format="%.6f")
 boat_latitude = st.number_input('Current Boat\'s Latitude', value=40.783282, step=1e-7, format="%.6f")
 
-'''
-## Once we have these, let's call our API in order to retrieve a prediction
+# Dictionary containing the parameters for our API...
+params = dict(
+    pickup_datetime=date_time_position,
+    boat_longitude=boat_longitude,
+    boat_latitude=boat_latitude,
+    dropoff_longitude=dropoff_longitude,
+    dropoff_latitude= dropoff_latitude,
+    passenger_count= passenger_count,
+    )
 
-See ? No need to load a `model.joblib` file in this app, we do not even need to know anything about Data Science in order to retrieve a prediction...
+# URL to call
+url = ''
 
-🤔 How could we call our API ? Off course... The `requests` package 💡
-'''
+if st.button("Is this boat fishing"):
+    res = requests.get(url, params=params).json()
+    st.subheader(f"The estimated fare is ${round(res['fare'],2)}")
 
-url = 'https://taxifare.lewagon.ai/predict'
-
-if url == 'https://taxifare.lewagon.ai/predict':
-
-    st.markdown('Maybe you want to use your own API for the prediction, not the one provided by Le Wagon...')
-
-'''
-
-2. Let's build a dictionary containing the parameters for our API...
-
-3. Let's call our API using the `requests` package...
-
-4. Let's retrieve the prediction from the **JSON** returned by the API...
-
-## Finally, we can display the prediction to the user
-'''
+if st.button("What type of gear is this boat using"):
+    res = requests.get(url, params=params).json()
+    st.subheader(f"The estimated fare is ${round(res['fare'],2)}")
