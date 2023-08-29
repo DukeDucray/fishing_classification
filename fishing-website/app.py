72ef1e618b08361040f4ec6794a717d957b0f922
Grace Leung Shing <gs.leungshing@gmail.com>
2023-08-29 15:05:37 +0400

    Folium map with polyline

--- fishing-website/app.py
+++ fishing-website/app.py
@@ -2,7 +2,9 @@ import streamlit as st
 import pandas as pd
 import datetime
 import requests
+import folium
 
+from streamlit_folium import st_folium
 from PIL import Image
 
 # Title
@@ -50,10 +52,30 @@ params = dict(
 # URL to call
 url = ''
 
-if st.button("Is this boat fishing"):
+if st.button("Is this boat fishing?"):
     res = requests.get(url, params=params).json()
-    st.subheader(f"The estimated fare is ${round(res['fare'],2)}")
+    st.subheader(f"The boat is fishing${res['fare']}")
 
-if st.button("What type of gear is this boat using"):
+if st.button("What type of gear is this boat using?"):
     res = requests.get(url, params=params).json()
-    st.subheader(f"The estimated fare is ${round(res['fare'],2)}")
+    st.subheader(f"The boat is using ${res['fare']}")
+
+
+map = folium.Map(location=[37.4601908, 126.4406957],
+               zoom_start=15)
+
+place_lat = [37.4601928, 37.4593108, 37.4641108, 37.4611508]
+place_lng = [126.4406957, 126.4432957, 126.4476917, 126.4423957]
+
+points = []
+for i in range(len(place_lat)):
+    points.append([place_lat[i], place_lng[i]])
+
+for index,lat in enumerate(place_lat):
+    folium.Marker([lat,
+                   place_lng[index]],
+                  popup=('patient{} \n 74contacts'.format(index)),
+                 icon = folium.Icon(color='green',icon='plus')).add_to(map)
+folium.PolyLine(points, color='red').add_to(map)
+
+st_folium(map)
