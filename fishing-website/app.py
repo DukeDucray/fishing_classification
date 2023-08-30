d9c9de9be5bfb3cc9ec58fca8ea9e06a86b2d2f1
Grace Leung Shing <gs.leungshing@gmail.com>
2023-08-30 12:30:23 +0400

    Used url and created test, pending csv into params

--- fishing-website/app.py
+++ fishing-website/app.py
@@ -25,43 +25,63 @@ as well as predict the fishing gear used by the vessel''')
 st.set_option('deprecation.showfileUploaderEncoding', False)
 uploaded_file = st.file_uploader("Choose a CSV file", type="csv")
 if uploaded_file is not None:
-    data = pd.read_csv(uploaded_file)
-    st.write(data)
-
-    # URL to call
-    url = ''
+    df = pd.read_csv(uploaded_file)
+    st.write(df)
 
     if st.button("Check this boat"):
+        # Define the API endpoint URL
+        #base_url =  "https://fishingclassification-hfis4rtnsq-ew.a.run.app"
+        #endpoint = "/sample"
+
+        #params = {
+        #    "sample_request": df
+        #    }
+
+        #print( 'query :', f"{base_url}{endpoint}?sample_request=trawlers")
+
+        # Get API response into json() format
+        response = requests.get("https://fishingclassification-hfis4rtnsq-ew.a.run.app/sample?sample_request=trawlers").json()
+
+        # Convert response to Pandas Dataframe
+        data = pd.DataFrame(response)
+
+        # Extract lattitude and longitude from Dataframe
         place_lat=data["lat"].tolist()
         place_lng=data["lon"].tolist()
         num = round(len(place_lat)/2)
 
+        # Create folium map
         base_map = folium.Map(location=[place_lat[num], place_lng[num]], control_scale=True)
 
+        # Get coordinates for fishing events
         df_fishing = data[data['is_fishing']==1]
         fishing = list(zip(df_fishing.lat, df_fishing.lon))
 
+        # Get coordinates for fishing events
         df_not_fishing = data[data['is_fishing']==0]
         not_fishing = list(zip(df_not_fishing.lat, df_not_fishing.lon))
 
+        # Create markers for each fishing event
         for fish in fishing:
-            icon=folium.Icon(color='white', icon_color="yellow")
+            icon=folium.Icon(color='white', icon_color="red")
             folium.Marker(fish, icon=icon).add_to(base_map)
 
         for notfish in not_fishing:
-            icon=folium.Icon(color='white', icon_color="red")
+            icon=folium.Icon(color='white', icon_color="green")
             folium.Marker(notfish, icon=icon).add_to(base_map)
 
+        # Create line that connect points
         points = []
         for i in range(len(place_lat)):
             points.append([place_lat[i], place_lng[i]])
+        folium.PolyLine(locations=points, color='yellow').add_to(base_map)
 
-        folium.PolyLine(locations=points, color='green').add_to(base_map)
-
+        # Bounds so that the map autozooms
         sw = data[['lat', 'lon']].min().values.tolist()
         ne = data[['lat', 'lon']].max().values.tolist()
         base_map.fit_bounds([sw, ne])
 
+        # Displays map
         folium_static(base_map)
 
 
