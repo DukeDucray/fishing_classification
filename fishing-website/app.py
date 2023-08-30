5e256419da41c96c5a25606a0ec53e944b41cc18
Grace Leung Shing <gs.leungshing@gmail.com>
2023-08-30 04:54:22 +0400

    Folium with trajectory, markers fishing and not fishing

--- fishing-website/app.py
+++ fishing-website/app.py
@@ -4,7 +4,7 @@ import datetime
 import requests
 import folium
 
-from streamlit_folium import st_folium
+from streamlit_folium import st_folium, folium_static
 from PIL import Image
 
 # Title
@@ -32,29 +32,55 @@ if uploaded_file is not None:
 url = ''
 
 if st.button("Is this boat fishing?"):
-    res = requests.get(url, params=params).json()
-    st.subheader(f"The boat is fishing${res['fare']}")
+    #res = requests.get(url, params=params).json()
+    #st.subheader(f"The boat is fishing {res['result']}")
+    place_lat=data["lat"].tolist()
+    place_lng=data["lon"].tolist()
+
+    num = round(len(place_lat)/2)
+
+    map = folium.Map(location=[place_lat[num], place_lng[num]])
+
+    points = []
+    for i in range(len(place_lat)):
+        points.append([place_lat[i], place_lng[i]])
+
+    for index,lat in enumerate(place_lat):
+        folium.Marker([lat,
+                    place_lng[index]],
+                    popup=('patient{} \n 74contacts'.format(index)),
+                    icon = folium.Icon(color='green',icon='plus', icon_size=(15,15))).add_to(map)
+    folium.PolyLine(points, color='red').add_to(map)
+    folium_static(map)
+
+if st.button("Test map2"):
+    place_lat=data["lat"].tolist()
+    place_lng=data["lon"].tolist()
+    num = round(len(place_lat)/2)
+
+    base_map = folium.Map(location=[place_lat[num], place_lng[num]], control_scale=True)
+
+    df_fishing = data[data['is_fishing']==1]
+    fishing = list(zip(df_fishing.lat, df_fishing.lon))
+
+    df_not_fishing = data[data['is_fishing']==0]
+    not_fishing = list(zip(df_not_fishing.lat, df_not_fishing.lon))
+
+    for fish in fishing:
+        icon=folium.Icon(color='white', icon_color="yellow")
+        folium.Marker(fish, icon=icon).add_to(base_map)
+
+    for notfish in not_fishing:
+        icon=folium.Icon(color='white', icon_color="red")
+        folium.Marker(notfish, icon=icon).add_to(base_map)
+
+    points = []
+    for i in range(len(place_lat)):
+        points.append([place_lat[i], place_lng[i]])
+
+    folium.PolyLine(locations=points, color='green').add_to(base_map)
+    folium_static(base_map)
 
 if st.button("What type of gear is this boat using?"):
     res = requests.get(url, params=params).json()
-    st.subheader(f"The boat is using ${res['fare']}")
-
-
-map = folium.Map(location=[37.4601908, 126.4406957],
-               zoom_start=15)
-
-place_lat = [37.4601928, 37.4593108, 37.4641108, 37.4611508]
-place_lng = [126.4406957, 126.4432957, 126.4476917, 126.4423957]
-
-points = []
-for i in range(len(place_lat)):
-    points.append([place_lat[i], place_lng[i]])
-
-for index,lat in enumerate(place_lat):
-    folium.Marker([lat,
-                   place_lng[index]],
-                  popup=('patient{} \n 74contacts'.format(index)),
-                 icon = folium.Icon(color='green',icon='plus')).add_to(map)
-folium.PolyLine(points, color='red').add_to(map)
-
-st_folium(map)
+    st.subheader(f"The boat is using {res['result']}")
