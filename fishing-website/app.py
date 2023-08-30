a27b651ad492e6de9d23dfe542c6b4a0cffe6f10
Grace Leung Shing <gs.leungshing@gmail.com>
2023-08-30 08:15:57 +0400

    map3 with autozoom location on map

--- fishing-website/app.py
+++ fishing-website/app.py
@@ -81,6 +81,40 @@ if st.button("Test map2"):
     folium.PolyLine(locations=points, color='green').add_to(base_map)
     folium_static(base_map)
 
+if st.button("Test map3"):
+    place_lat=data["lat"].tolist()
+    place_lng=data["lon"].tolist()
+    num = round(len(place_lat)/2)
+
+    base_map = folium.Map(location=[place_lat[num], place_lng[num]], control_scale=True)
+
+    df_fishing = data[data['is_fishing']==1]
+    fishing = list(zip(df_fishing.lat, df_fishing.lon))
+
+    df_not_fishing = data[data['is_fishing']==0]
+    not_fishing = list(zip(df_not_fishing.lat, df_not_fishing.lon))
+
+    for fish in fishing:
+        icon=folium.Icon(color='white', icon_color="yellow")
+        folium.Marker(fish, icon=icon).add_to(base_map)
+
+    for notfish in not_fishing:
+        icon=folium.Icon(color='white', icon_color="red")
+        folium.Marker(notfish, icon=icon).add_to(base_map)
+
+    points = []
+    for i in range(len(place_lat)):
+        points.append([place_lat[i], place_lng[i]])
+
+    folium.PolyLine(locations=points, color='green').add_to(base_map)
+
+    sw = data[['lat', 'lon']].min().values.tolist()
+    ne = data[['lat', 'lon']].max().values.tolist()
+    base_map.fit_bounds([sw, ne])
+
+    folium_static(base_map)
+
+
 if st.button("What type of gear is this boat using?"):
     res = requests.get(url, params=params).json()
     st.subheader(f"The boat is using {res['result']}")
